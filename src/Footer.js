7b8bd3612a8317976799c624d8a23c3d7d40c84a
Saranghitta <flores.manuelh08@gmail.com>
2023-11-12 18:23:22 -0800

    added semantic html and meta tags

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Footer (){
+    return(
+        <footer>
+             <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/reserve">Reservations</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/order">Order Online</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
